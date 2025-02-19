a6635801005ad6954ef502878c5f1a8210776b7d
Kirill Kaimusov <kaimusovks@yandex.ru>
2025-02-19 20:32:51 +0500

    Add interface for Cart.ts, change Cart.ts

--- src/ts/service/Cart.ts
+++ src/ts/service/Cart.ts
@@ -1,28 +1,23 @@
-export class Cart {
-    id: number;
-    name: string;
-    price: number;
-    discount: number;
+import Buyable from "../domain/Buyable";
+export class Cart implements Buyable {
+    constructor (
+        public readonly id: number,
+        public name: string,
+        public price: number,
+        public discount: number
+    ) {}
+    private items: Buyable[] = [];
 
-    private _items: {id: number, name: string, price: number, discount: number}[] = [];
-
-    constructor(id: number, name: string, price: number, discount: number) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.discount = discount
+    add(item: Buyable): void {
+        this.items.push(item);
     }
 
-    add(item: {id: number, name: string, price: number, discount: number}): void {
-        this._items.push(item);
-    }
-
-    get() {
-        return [...this._items];
+    get(): Buyable[] {
+        return [...this.items];
     }
 
     calculateTotal(): number {
-        return this._items.reduce((total, item) => total + item.price, 0);
+        return this.items.reduce((total, item) => total + item.price, 0);
     }
 
     calculateTotalWithoutDiscount(): number {
@@ -30,10 +25,10 @@ export class Cart {
     }
 
     calculateTotalWithDiscount(): number {
-        return this._items.reduce((total, item) => total + item.price * (1 - item.discount / 100), 0);
+        return this.items.reduce((total, item) => total + item.price * (1 - item.discount / 100), 0);
     }
 
     removeItemById(id: number): void {
-        this._items = this._items.filter(item => item.id !== id);
+        this.items = this.items.filter(item => item.id !== id);
     }
 }
